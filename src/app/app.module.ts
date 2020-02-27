9e2fe833e14688576b8daf9e2edf02201f170034
SomeOne583 <janel.fantasma2002@gmail.com>
2020-02-27 01:54:06 -0600

    Working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatStepperModule } from '@angular/material/stepper';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import { MatStepperModule } from '@angular/material/stepper';
     MatIconModule,
     MatCardModule,
     MatExpansionModule,
-    MatStepperModule
+    MatStepperModule,
+    MatListModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
