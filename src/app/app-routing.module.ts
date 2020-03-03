0d7a8bce9933795010eb4c323f812103d2931a2a
SomeOne583 <janel.fantasma2002@gmail.com>
2020-03-02 22:30:35 -0600

    Heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 
-const routes: Routes = [{
-  path: "**", component: MainComponent,
-}];
+const routes: Routes = [
+  { path: "", component: MainComponent },
+  { path: "**", component: MainComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
